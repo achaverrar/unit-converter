ee95ed0d4cc6950ada310c8cd822357e239d6d79
Ayxa Chaverra <achaverrar@gmail.com>
2023-03-11 11:41:23 -0500

    create variable to store minimum valid value in each unit

--- src/application/converters/BaseUnitConverter.java
+++ src/application/converters/BaseUnitConverter.java
@@ -8,20 +8,21 @@ import application.InvalidInputException;
 
 public abstract class BaseUnitConverter {
 	protected BigDecimal MULTIPLIER;
-	
+	private BigDecimal MINIMUM_VALID_VALUE = new BigDecimal("0.0");
+
 	public abstract String getName();
-	
+
 	public BigDecimal convertToBase(String input) throws InvalidInputException {
 		BigDecimal value = formatInput(input);
-		// up to 4 decimal places, approximate to the next unit from .5 
+		// up to 4 decimal places, approximate to the next unit from .5
 		return value.multiply(MULTIPLIER).setScale(4, RoundingMode.HALF_UP);
 	}
-	
+
 	public BigDecimal convertFromBase(String input) throws InvalidInputException {
 		BigDecimal value = formatInput(input);
 		return value.divide(MULTIPLIER, 4, RoundingMode.HALF_UP);
 	}
-	
+
 	public BigDecimal formatInput(String input) throws InvalidInputException {
 		try {
 			return new BigDecimal(Double.parseDouble(input));
