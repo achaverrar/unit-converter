4e348f4e7164b71cbf8b69a3e0f4802ff187e2cc
Ayxa Chaverra <achaverrar@gmail.com>
2023-03-12 22:09:00 -0500

    convert the speedConverters BaseUnitConverter array into a FullRangeBaseUnitConverter array so that speed converters allow negative input values

--- src/application/converters/speed/SpeedConverter.java
+++ src/application/converters/speed/SpeedConverter.java
@@ -3,13 +3,14 @@ package application.converters.speed;
 import java.util.HashMap;
 
 import application.converters.BaseUnitConverter;
+import application.converters.FullRangeBaseUnitConverter;
 import application.converters.UnitTypeConverter;
 
 public class SpeedConverter extends UnitTypeConverter {
 
-	private static BaseUnitConverter[] speedConverters = new BaseUnitConverter[] {
-			new BaseUnitConverter("Kilometers / Hour", 0.277778), new BaseUnitConverter("Knots", 0.514444),
-			new BaseUnitConverter("Meters / Second", 1.0), new BaseUnitConverter("Miles / Hour", 0.44704), };
+	private static FullRangeBaseUnitConverter[] speedConverters = new FullRangeBaseUnitConverter[] {
+			new FullRangeBaseUnitConverter("Kilometers / Hour", 0.277778), new FullRangeBaseUnitConverter("Knots", 0.514444),
+			new FullRangeBaseUnitConverter("Meters / Second", 1.0), new FullRangeBaseUnitConverter("Miles / Hour", 0.44704), };
 
 	public SpeedConverter() {
 		super("Meters / Second", speedConverters);
