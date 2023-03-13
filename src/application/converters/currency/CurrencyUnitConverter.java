c4a31e7ee202e3b6ff054040f5582bf3144455db
Ayxa Chaverra <achaverrar@gmail.com>
2023-03-13 11:59:46 -0500

    fix bug with currency conversions by overriding conversion methods in the class CurrencyUnitConverter

--- src/application/converters/currency/CurrencyUnitConverter.java
+++ src/application/converters/currency/CurrencyUnitConverter.java
@@ -1,7 +1,9 @@
 package application.converters.currency;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+import application.InvalidInputException;
 import application.converters.PartialRangeUnitConverter;
 
 public class CurrencyUnitConverter extends PartialRangeUnitConverter {
@@ -14,6 +16,26 @@ public class CurrencyUnitConverter extends PartialRangeUnitConverter {
 	
 	public void setConversionFactor(BigDecimal newConversionFactor) {
 		this.CONVERSION_FACTOR = newConversionFactor;
+	}
+	
+	@Override
+	public BigDecimal convertToBase(String input) throws InvalidInputException {
+		BigDecimal value = formatInput(input);
+		// up to 4 decimal places, approximate to the next unit from .5
+		validateInputNumber(value);
+		return value.divide(CONVERSION_FACTOR, 4, RoundingMode.HALF_UP);
+	}
+
+	@Override
+	public BigDecimal convertFromBase(String input) throws InvalidInputException {
+		BigDecimal value = formatInput(input);
+		validateInputNumber(value);
+		return value.multiply(CONVERSION_FACTOR).setScale(4, RoundingMode.HALF_UP);
+	}
+	
+	@Override
+	public BigDecimal convertFromBase(BigDecimal value) {
+		return value.multiply(CONVERSION_FACTOR).setScale(4, RoundingMode.HALF_UP);
 	}
 	
 	public String getCurrencyCode() {
