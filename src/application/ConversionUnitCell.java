1bae25d3d9914beefcc4c8faba5cb0a13975156b
Ayxa Chaverra <achaverrar@gmail.com>
2023-03-19 01:28:53 -0500

    add the classes ConversionUnitCellFactory and ConversionUnitCell to customize the appearance of the left and right hand side comboboxes

---
+++ src/application/ConversionUnitCell.java
@@ -0,0 +1,32 @@
+package application;
+
+import application.converters.BaseUnitConverter;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ConversionUnitCell extends ListCell<BaseUnitConverter>{
+
+	@Override
+	public void updateItem(BaseUnitConverter item, boolean empty)
+	{
+		super.updateItem(item, empty);
+
+		if (empty)
+		{
+			setText(null);
+			setGraphic(null);
+		}
+		else
+		{
+			setText(item.getName());
+			//Image image1 = new Image(getClass().getResourceAsStream("/images/converters/"+item.getIconName()));
+			Image image1 = new Image(getClass().getResourceAsStream("/images/flags/COP.png"));
+			ImageView imageView1 = new ImageView(image1);
+			imageView1.setPreserveRatio(true);
+			imageView1.setFitWidth(48); 
+			setGraphic(imageView1);
+		}
+	}
+
+}
