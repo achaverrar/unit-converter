0100e4135542595026f30367d1fb3ee147f0f14d
Ayxa Chaverra <achaverrar@gmail.com>
2023-03-19 22:16:06 -0500

    translate all the text from the class VolumeConverter that is going to be displayed in the UI

--- src/application/converters/volume/VolumeConverter.java
+++ src/application/converters/volume/VolumeConverter.java
@@ -7,19 +7,20 @@ import application.converters.UnitTypeConverter;
 
 public class VolumeConverter extends UnitTypeConverter {
 
-	private static PartialRangeUnitConverter[] volumeConverters = new PartialRangeUnitConverter[] { new PartialRangeUnitConverter("Cups", 0.24),
-			new PartialRangeUnitConverter("Fluid Ounces", 0.0295735), new PartialRangeUnitConverter("Gallons", 3.78541),
-			new PartialRangeUnitConverter("Liters", 1.0), new PartialRangeUnitConverter("Milliliters", 0.001),
-			new PartialRangeUnitConverter("Pints", 0.473176), new PartialRangeUnitConverter("Quarts", 0.946353),
-			new PartialRangeUnitConverter("Tablespoons", 0.0147868), new PartialRangeUnitConverter("Teaspoons", 0.00492892) };
+	private static PartialRangeUnitConverter[] volumeConverters = new PartialRangeUnitConverter[] {
+			new PartialRangeUnitConverter("Tazas", 0.24), new PartialRangeUnitConverter("Onzas Líquidas", 0.0295735),
+			new PartialRangeUnitConverter("Galones", 3.78541), new PartialRangeUnitConverter("Litros", 1.0),
+			new PartialRangeUnitConverter("Mililitros", 0.001), new PartialRangeUnitConverter("Pintas", 0.473176),
+			new PartialRangeUnitConverter("Cuartos", 0.946353), new PartialRangeUnitConverter("Cucharadas", 0.0147868),
+			new PartialRangeUnitConverter("Cucharaditas", 0.00492892) };
 
 	public VolumeConverter() {
-		super("Liters", volumeConverters);
+		super("Litros", volumeConverters);
 	}
 
 	@Override
 	public String getUnitType() {
-		return "Volume";
+		return "Volumen";
 	}
 
 	@Override
